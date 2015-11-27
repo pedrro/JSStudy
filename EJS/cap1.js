e9b0ba68fd32785463fa85190d516c2c453e03fd
Pedro Silva <pedrro2@hotmail.com>
2015-11-27 11:16:47 -0200

    EJS - cap 1 - looping a triangle

---
+++ EJS/cap1.js
@@ -0,0 +1,24 @@
+
+function Triangle(){
+}
+
+Triangle.prototype.make = function(height){
+  var triangle = '';
+  var count= 1;
+
+  for(var i =1; i <= height;i++){
+    triangle += _repeat('#',i) + '\n';
+  }
+  
+  return triangle;
+};
+
+var _repeat = function(char, index){
+  var result = '';
+  for(var i = 0; i < index; i++){
+    result += char;
+  }
+    return result;
+};
+
+module.exports = Triangle;
