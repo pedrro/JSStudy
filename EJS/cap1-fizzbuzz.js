e97e6ffd1a37836b836a3d85c596c7e128eacaf5
Pedro Silva <pedrro2@hotmail.com>
2015-12-04 10:33:28 -0200

    refactoring following eslint rules

--- EJS/cap1-fizzbuzz.js
+++ EJS/cap1-fizzbuzz.js
@@ -1,16 +1,16 @@
-function FizzBuzz(){
+function FizzBuzz() {
 
 }
 
-FizzBuzz.prototype.checkFizzBuzz = function(num){
-  if(num % 3 === 0 && num % 5 === 0){
+FizzBuzz.prototype.checkFizzBuzz = function (num) {
+  if (num % 3 === 0 && num % 5 === 0) {
     return 'FizzBuzz';
-  }else if(num % 3 === 0){
+  } else if (num % 3 === 0) {
     return 'Fizz';
-  } else if (num % 5 === 0){
+  } else if (num % 5 === 0) {
     return 'Buzz';
   }
-}
+};
 
 
 
--- spec/cap1-fizzbuzz-spec.js
+++ spec/cap1-fizzbuzz-spec.js
@@ -1,21 +1,21 @@
 var FizzBuzz = require('../EJS/cap1-fizzbuzz.js');
 
-describe('Exercise - FIZZBUZZ - of cap1 of Eloquent JS', function(){
-var fizzbuzz;
+describe('Exercise - FIZZBUZZ - of cap1 of Eloquent JS', function () {
+  var fizzbuzz;
 
-beforeEach(function(){
+  beforeEach(function () {
   fizzbuzz = new FizzBuzz();
 });
 
-it('should show Fizz', function(){
+  it('should show Fizz', function () {
   expect('Fizz').toBe(fizzbuzz.checkFizzBuzz(3));
 });
 
-it('should show Buzz', function(){
+  it('should show Buzz', function () {
   expect('Buzz').toBe(fizzbuzz.checkFizzBuzz(5));
 });
 
-it('should show FizzBuzz', function(){
+  it('should show FizzBuzz', function () {
   expect('FizzBuzz').toBe(fizzbuzz.checkFizzBuzz(30));
 });
 
